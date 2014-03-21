c8f93c924b3070b12635a91fad0482a12771eb94
BLNorris <BLNorris@gmail.com>
2014-03-21 16:09:08 -0500

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flipbook::Application.config.secret_token = 'fd2c39a43c62e825a05bff89063d66c59c4f8e3f102ae7c239f802746923c9bd3b0ead79b32a2d810b7d1d96ecc72dc8dac476b0b4a80e73dc5654f451e483fd'
