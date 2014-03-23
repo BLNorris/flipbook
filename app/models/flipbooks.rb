217f3efd41e9071ff3c426beb640d325185abaee
BLNorris <BLNorris@gmail.com>
2014-03-23 11:22:24 -0500

    generated models

---
+++ app/models/flipbooks.rb
@@ -0,0 +1,3 @@
+class Flipbooks < ActiveRecord::Base
+  attr_accessible :description, :name, :user_id
+end
---
+++ app/models/pictures.rb
@@ -0,0 +1,3 @@
+class Pictures < ActiveRecord::Base
+  attr_accessible :description, :flipbook_id, :url
+end
