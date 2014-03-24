6029cefdb9de9567afa9072035dc74a314260884
BLNorris <BLNorris@gmail.com>
2014-03-24 09:46:17 -0500

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :email, :name, :password, :password_confirmation
-  validates :email, :uniqueness => true
+  validates :email, :name, :password, :password_confirmation, presence: true
+  validates :email, :uniqueness => true, email: true
   validates :password, length: { minimum: 6 }
 end
