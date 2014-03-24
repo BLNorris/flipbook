f3079a24a5405ba00b6dbcb9c8097727da648111
BLNorris <BLNorris@gmail.com>
2014-03-23 19:32:51 -0500

    more login things

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-   has_secure_password
+  has_secure_password
   attr_accessible :email, :name, :password, :password_confirmation
   validates :email, :uniqueness => true
+  validates :password, length: { minimum: 6 }
 end
