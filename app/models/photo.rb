72324bdeb6781fcd94c9bec50889d004abcbc105
Fara Britt Woolf <fbwoolf@gmail.com>
2014-03-25 10:26:24 -0500

    Adding model associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :description, :flipbk_id, :url
+  attr_accessible :description, :flipbk_id, :url, :user_id
   
   belongs_to :flipbk
+  belongs_to :user
 end
