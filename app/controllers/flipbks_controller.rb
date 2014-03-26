a3cbe4d19e3ed75f150f7d60ea884f1a5ad234c2
BLNorris <BLNorris@gmail.com>
2014-03-26 14:55:38 -0500

    all flipbooks are functional and stuff

--- app/controllers/flipbks_controller.rb
+++ app/controllers/flipbks_controller.rb
@@ -22,9 +22,32 @@ class FlipbksController < ApplicationController
   end
 
   def update
+    @book = Flipbk.find(params[:id])
+    @book.update_attributes(params[:flipbk])
+    @book.user_id = session[:user_id]
+    
+    if @book.save
+      Photo.all.each do |p|
+        if p.flipbk_id == @book.id
+          p.flipbk_id = nil
+          p.save
+        end
+      end
+      if params[:photos]      
+        params[:photos].each do |p|
+          photo = Photo.find(p)
+          photo.flipbk_id = @book.id
+          photo.save
+        end
+      end
+      redirect_to(flipbk_path(@book.id))
+    else
+      render "edit"
+    end
   end
 
   def edit
+    @book = Flipbk.find(params[:id])
   end
 
   def destroy
@@ -33,16 +56,32 @@ class FlipbksController < ApplicationController
       p.flipbk_id = nil
     end
     book.delete
-    redirect_to(:flipbks)
+    redirect_to(:users)
   end
 
   def index
     @user = User.find(session[:user_id])
+
   end
 
   def show
-    @user = User.find(session[:user_id])
+    
+    
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
     @book = Flipbk.find(params[:id])
+    
+    if @book.public?
+      puts "ITS PUBLIC YO"
+    
+    else
+      if current_user &&@book.user.id == current_user.id
+      else
+      redirect_to(:root)
+      end
+    end
+    
   end
   
 
