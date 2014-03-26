c29585ee0d2bf763cc3db454db1b44e959162b2d
BLNorris <BLNorris@gmail.com>
2014-03-26 16:47:45 -0500

    fixed the thing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,7 @@
 class PhotosController < ApplicationController
   
   def feed
-    @client = Instagram.client(:access_token => session[:access_token])
-   
-    
-    for media_item in @client.user_recent_media
-      photo_url = media_item.images.standard_resolution.url
-      photo = Photo.create(:url => photo_url , :user_id => session[:user_id]) 
-    end
+
   end
   
 end
