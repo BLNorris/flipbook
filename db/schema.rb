5b238e6567bc80a64bd3df6ec7c49fd4b406ba22
BLNorris <BLNorris@gmail.com>
2014-03-27 14:11:22 -0500

    added urs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140326160015) do
+ActiveRecord::Schema.define(:version => 20140327190908) do
 
   create_table "flipbks", :force => true do |t|
     t.integer  "user_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20140326160015) do
     t.datetime "updated_at",  :null => false
     t.integer  "speed"
     t.boolean  "public"
+    t.string   "url"
   end
 
   create_table "photos", :force => true do |t|
